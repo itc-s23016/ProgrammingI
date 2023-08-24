5c900550aaf2afd3aab3271e460040ac389661d1
Namito Tsuhako <s23016@std.it-college.ac.jp>
2023-08-24 06:01:47 +0000

    練習課題

---
+++ CHAPTER05/Q5_3_1.py
@@ -0,0 +1,3 @@
+members = ["Yamada", "Satou", "Suzuki", "Tanaka", "Itou"]
+del members[2]  # del文で'membersのインデックス２の'Suzuki'を削除した。
+print(members)
---
+++ CHAPTER05/Q5_3_2.py
@@ -0,0 +1,4 @@
+x = list(range(1, 6))
+print(x)
+del x[-2:]  # del文でｘのリストから末尾の２つの要素を指定して、削除した。
+print(x)
